81cfd620746d7254e9ee78505239fae40b13ef7e
Pasindu Navodya <pasinavod@gmail.com>
2025-08-13 02:14:07 +0530

    firebase

--- watchscape-client/src/firebase.js
+++ watchscape-client/src/firebase.js
@@ -1,15 +1,16 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAgFsBKhQsegX1fAhoOpXE2e1itgvYTrLw",
+  authDomain: "watchscape-b7932.firebaseapp.com",
+  projectId: "watchscape-b7932",
+  storageBucket: "watchscape-b7932.appspot.com",
+  messagingSenderId: "331344730098",
+  appId: "1:331344730098:web:311e6b9a80cb91a3f87d6b",
+  measurementId: "G-V70YRX5VEP"
 };
 
 const app = initializeApp(firebaseConfig);
