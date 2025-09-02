1a6ee831c85937ffcc698d3cd0c04a3356bc1e8d
Pasindu Navodya <pasinavod@gmail.com>
2025-09-02 10:59:14 +0530

    write something

--- watchscape-client/src/pages/Home.jsx
+++ watchscape-client/src/pages/Home.jsx
@@ -241,7 +241,7 @@ export default function Home({ user }) {
                 <UserIcon className="w-5 h-5 text-white" />
               </div>
               <div className="flex-1 py-3 px-4 bg-gray-100 rounded-full text-gray-500">
-                Write something or attach a movie...
+                Write something ...
               </div>
               <PencilSquareIcon className="w-6 h-6 text-purple-600" />
             </div>
